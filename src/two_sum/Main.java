6eece2165259789b5e18839975d3e5d3049c9992
wesley ramos <wesleyramos523@gmail.com>
2020-09-20 11:40:55 -0300

    two_sum

---
+++ src/two_sum/Main.java
@@ -0,0 +1,13 @@
+package two_sum;
+
+public class Main {
+    //https://leetcode.com/problems/two-sum/
+    public static void main(String[] args) {
+        int[] nums = new int[]{-1, -2, -3, -4, -5};
+        int target = -8;
+        int[] sol = Solution.twoSum(nums, target);
+        for (int i = 0; i < sol.length; i++) {
+            System.out.println("Element at index " + i + " : " + sol[i]);
+        }
+    }
+}
---
+++ src/two_sum/Solution.java
@@ -0,0 +1,20 @@
+package two_sum;
+
+public class Solution {
+
+    public static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            {
+                for (int j = i + 1; j < nums.length; j++) {
+                    if (target == (nums[i] + nums[j])) {
+                        return new int[]{i, j};
+                    }
+                }
+            }
+
+        }
+
+        return null;
+    }
+}
+
